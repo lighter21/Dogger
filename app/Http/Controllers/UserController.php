572b1df7bb3aef34b761a6d1b44ab1815f5703ae
astrzoda1 <61603478+astrzoda1@users.noreply.github.com>
2021-05-08 16:21:21 +0200

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,99 +1,48 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\User;
 use Illuminate\Support\Facades\Auth;
-use App\Pet;
 use Illuminate\Http\Request;
 
-class PetController extends Controller
+class UserController extends Controller
 {
     public function __construct()
     {
         $this->middleware('auth');
     }
-    public function create()
+    private function validateRequest(Request $rq)
     {
-        return view('pet.create');
-    }
-    public function validation(Request $request)
-    {
-        $this->validate($request, [
-            'name' => 'required|min:2|max:30',
-            'type' => 'required',
-            'breed' => 'required',
-            'file' => 'nullable|mimes:jpeg,bmp,png'
+        $this->validate($rq, [
+            'street' => 'required|min:2|max:50',
+            'house_number' => 'required|min:1|max:10',
+            'postcode' => 'required|min:2|max:15',
+            'city' => 'required|min:2|max:80',
+            'phone_number' => 'required|numeric|min:8'
         ]);
-        return $request;
-    }
-    public function store(Request $request)
-    {
-        $this->validate($request, [
-            'name' => 'required',
-            'type' => 'required',
-            'breed' => 'required',
-            'file' => 'nullable|mimes:jpeg,bmp,png'
-        ]);
-        
-        if ($request->hasFile('file')) 
-        {
-            //storage/app/public/pet
-            $request->file->store('pet', 'public');
-            Pet::create([
-                "user_id" => $userId = Auth::id(),
-                "name" => $request->name,
-                "type" => $request->type,
-                "breed" => $request->breed,
-                "file_path" => $request->file->hashName()
-            ]);
-        }
-        else
-        {
-            $request->file = "brak.png";
-            Pet::create([
-                "user_id" => $userId = Auth::id(),
-                "name" => $request->name,
-                "type" => $request->type,
-                "breed" => $request->breed,
-                "file_path" => $request->file
-            ]);
-        }
-
-        return redirect('home')->with('message', 'Zwierzak został dodany!');
-    }
-    public function index()
-    {
-        $userId = auth()->user()->id;
-        $databaseData = Pet::where('user_id',$userId)->get();
-        return view('pet/index', ['databaseData' => $databaseData]);
-    }
-    public function destroy($id)
-    {
-        $databaseData=Pet::find($id);
-        $databaseData -> delete();
-        return redirect('index');
+        return $rq;
     }
 
-    public function edit($id)
+    public function edit()
     {
-        $databaseData=Pet::find($id);
-        return view('pet.edit', ['databaseData' => $databaseData]);
+        $id = Auth::id();
+        $databaseData=User::find($id);
+        return view('user.edit', ['databaseData' => $databaseData]);
     }
-    public function update($id, Request $request)
+    public function update(Request $request)
     {
-        $validatedRequest=$this->validation($request);
-        $invoice=Pet::find($id);
-        $invoice->name=$validatedRequest->name;
-        $invoice->type=$validatedRequest->type;
-        $invoice->breed=$validatedRequest->breed;
-        if ($request->hasFile('file')) 
-        {
-            //storage/app/public/pet
-            $request->file->store('pet', 'public');
-            $invoice->file_path=$validatedRequest->file->hashName();
-        }
-        $invoice->save();
+        $validatedRequest = $this->validateRequest($request);
+        $id = Auth::id();
 
-        return redirect('index');
+        $user=User::find($id);
+        $user->street = $validatedRequest->street;
+        $user->house_number = $validatedRequest->house_number;
+        $user->postcode = $validatedRequest->postcode;
+        $user->city = $validatedRequest->city;
+        $user->phone_number = $validatedRequest->phone_number;
+        $user->save();
+
+        return redirect('home');
     }
 
 }
