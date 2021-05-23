57588d2ae0204806ae8d26e6323c196dbf1a4a1e
lukigu <82062009+lukigu@users.noreply.github.com>
2021-05-23 12:20:15 +0200

    Doladowanie dogger coinow
    user/addCoins

---
+++ resources/views/user/addCoins.blade.php
@@ -0,0 +1,59 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+                <div class="text-center p-2 mb-2 text-6xl font-bold">Doładuj Dogger Coiny!</div>
+
+                    <div class="card-body">
+    
+                        <form method="POST" action="" enctype ="multipart/form-data">
+                            @csrf
+
+                            <div class="form-group row">
+                                <label for="user" class="col-md-4 col-form-label text-md-right">Użytkownik:</label>
+
+                                <div class="col-md-6">
+                                    <input id="user" type="text" class="form-control" name="user" value="<?php
+                                        use Illuminate\Support\Facades\Auth;
+                                    if (Auth::check())
+                                    {
+                                        $user = Auth::user();
+                                        if (isset($user->name)) {
+                                            print($user->name);
+                                        }
+                                    }
+                                    ?>
+                                    " readonly>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="breed" class="col-md-4 col-form-label text-md-right">Ilość Coinów:</label>
+
+                                <div class="col-md-6">
+                                    <select id="breed" name="breed" class="form-control @error('breed') is-invalid @enderror" required autocomplete="breed" autofocus>
+                                            <option value="10">10 - 10zł</option>
+                                            <option value="25">25 - 24zł</option>
+                                            <option value="50">50 - 47zł</option>
+                                            <option value="100">100 - 90zł</option>
+                                            <option value="200">200 - 175zł</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="form-group row mb-0">
+                                <div class="col-md-6 offset-md-4 mt-4 text-center">
+                                    <button type="submit" class="btn btn-primary">
+                                        Doładuj Dogger Coiny
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                        
+                    </div>
+        </div>
+        <div class="text-center p-2 mb-2 mt-4 text-4xl font-bold">Obsługiwane płatności:</div>
+        <img class="h-100% w-100% m-4" src="/img/payment_methods.png" alt="no payment method">
+    </div>
+</div>
+    @endsection
