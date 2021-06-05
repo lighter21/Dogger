e9f9a42e1d9ac9836dd72dc59816c23f6c35be0c
ravl0 <56367465+ravl0@users.noreply.github.com>
2021-06-05 22:13:57 +0200

    Zmiana wygladu formularza, na wzór pozostałych. Jest jednak problem po kliknięciu, gdyż tutaj kolor okna robi się czarny, a w reszcie jest niebieski.

--- resources/views/walk/create.blade.php
+++ resources/views/walk/create.blade.php
@@ -1,56 +1,66 @@
 @extends('layouts.app')
 @section('content')
-    <div class="flex items-center h-screen w-full">
-        <div class="w-full bg-white rounded shadow-lg p-8 m-4">
-            <h1 class="block w-full text-center text-gray-400 mb-6">Dodaj ogłoszenie</h1>
-            @isset($myPets)
-                @if(session()->has('error'))
-                    <p>{{session('error')}}</p>
-                @endif
-                <form class="mb-4 flex flex-wrap justify-between" method="post" action="{{route('storeWalk')}}">
-                    @csrf
-                    <div class="flex flex-col mb-4 md:w-1/2">
-                        <label class="mb-2 uppercase tracking-wide font-bold text-lg text-gray-400" for="data">
-                            Zwierzak </label>
-                        <select class="border py-2 px-3 md:mr-2" name="pet_id">
-                            @foreach($myPets as $pet)
-                                <option value="{{$pet->id}}">{{$pet->name}}</option>
-                            @endforeach
-                        </select>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Dodaj Ogłoszenie</div>
+                    <div class="card-body">
+                        @isset($myPets)
+                            @if(session()->has('error'))
+                                <p>{{session('error')}}</p>
+                            @endif
+                            <form method="POST" action="{{route('storeWalk')}}">
+                                @csrf
+                                <div class="form-group row">
+                                    <label class="col-md-4 col-form-label text-md-right" for="data">Zwierzak</label>
+                                    <select class="col-md-6" name="pet_id">
+                                        @foreach($myPets as $pet)
+                                            <option value="{{$pet->id}}">{{$pet->name}}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                                @error('pet_id')
+                                <p>{{$message}}</p>
+                                @enderror
+                                {{--            To pole daty, deskrypcji spaceru bedzie dodane troche pozniej do bazy, na razie robimy podstawe. Ale sformatujcie to juz teraz, potem nie trzeba bedzie wracać--}}
+                                <div class="form-group row">
+                                    <label class="col-md-4 col-form-label text-md-right" for="date"> Data odbycia
+                                        spaceru </label>
+                                    <input class="border py-2 px-3" name="date" type="datetime-local">
+                                </div>
+                                @error('data')
+                                <p>{{$message}}</p>
+                                @enderror
+                                <div class="form-group row">
+                                    <label class="col-md-4 col-form-label text-md-right" for="description"> Dodatkowe
+                                        informacje dla wyprowadzającego </label>
+                                    <textarea class="border py-2 px-3" name="description" type="datetime-local"> </textarea>
+                                </div>
+                                @error('description')
+                                <p>{{$message}}</p>
+                                @enderror
+                                <div class="form-group row">
+                                    <label class="col-md-4 col-form-label text-md-right" for="description"> Stawka </label>
+                                    <input name="payment" class="border py-2 px-3" type="number">
+                                </div>
+                                <div class="form-group row mb-0">
+                                    <div class="col-md-6 offset-md-4">
+                                        <button type="submit" class="btn btn-primary">
+                                            Dodaj ogłoszenie
+                                        </button>
+                                    </div>
+                                </div>
+                                @endisset
+                            </form>
+                            @empty($myPets)
+                                <p>Aby dodać spacer, musisz dodać swojego zwierzaka <a href="{{route('createPet')}}">tutaj</a></p>
+                            @endempty
                     </div>
-                    @error('pet_id')
-                    <p>{{$message}}</p>
-                    @enderror
-                    {{--            To pole daty, deskrypcji spaceru bedzie dodane troche pozniej do bazy, na razie robimy podstawe. Ale sformatujcie to juz teraz, potem nie trzeba bedzie wracać--}}
-                    <div class="flex flex-col mb-4 md:w-1/2">
-                        <label class="mb-2 uppercase font-bold text-lg text-gray-400 md:ml-2" for="date"> Data odbycia
-                            spaceru </label>
-                        <input class="border py-2 px-3 md:ml-2" name="date" type="datetime-local">
                     </div>
-                    @error('data')
-                    <p>{{$message}}</p>
-                    @enderror
-                    <div class="flex flex-col mb-4 w-full">
-                        <label class="mb-2 uppercase font-bold text-lg text-gray-400" for="description"> Dodatkowe
-                            informacje dla wyprowadzającego </label>
-                        <textarea class="border py-2 px-3" name="description" type="datetime-local"> </textarea>
-                    </div>
-                    @error('description')
-                    <p>{{$message}}</p>
-                    @enderror
-                    <div class="flex flex-col mb-6 w-full">
-                        <label class="mb-2 uppercase font-bold text-lg text-gray-400" for="description"> Stawka </label>
-                        <input name="payment" class="border py-2 px-3" type="number">
-                    </div>
-                    <button
-                        class="block bg-green-400 hover:bg-green-500 text-white uppercase text-lg mx-auto p-4 rounded"
-                        type="submit">Dodaj ogłoszenie
-                    </button>
-                    @endisset
-                </form>
-                @empty($myPets)
-                    <p>Aby dodać spacer, musisz dodać swojego zwierzaka <a href="{{route('createPet')}}">tutaj</a></p>
-                @endempty
+                </div>
+            </div>
         </div>
     </div>
+
 @endsection
